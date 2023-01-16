8b2523f504894dff77beca41a43be549e7137026
Kevin Huang <33188761+KevinH48264@users.noreply.github.com>
2023-01-15 22:55:43 -0500

    updated version showing answers

--- src/contentScript/addPromptSearchListener.ts
+++ src/contentScript/addPromptSearchListener.ts
@@ -15,6 +15,8 @@ export const addPromptSearchListener = () => {
       var textareabox = item as HTMLTextAreaElement
       if (textareabox.value) {
         promptText = textareabox.value as string;
+      } else {
+        promptText = ""
       }
 
       reloadPopover(item, promptText)
@@ -22,9 +24,10 @@ export const addPromptSearchListener = () => {
 
     // TODO: how to figure out when something is clicked
     // save prompt in local storage
-    var button = document.getElementsByClassName('absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1 md:bottom-2.5 md:right-2 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent')
-    if (item == button[0]) {
+    var button = document.getElementsByClassName('flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]')
+    if (button[0].contains(item) || button[0] == item) {
       savePrompt(promptText)
+      promptText = ""
     }
   });
 
@@ -34,18 +37,28 @@ export const addPromptSearchListener = () => {
       // save to local chrome storage
       var textareabox = item as HTMLTextAreaElement
       if (textareabox.value) {
-        promptText = textareabox.value + event.key as string;
+        if (event.key != "Backspace") {
+          promptText = textareabox.value + event.key as string;
+        } else {
+          promptText = textareabox.value.substring(0, textareabox.value.length - 1) as string;
+        }
       }
 
       // only reload if you've typed at least one word?
       if (event.key == " ") {
         reloadPopover(item, promptText)
       }
+      if (event.key == "Backspace" && textareabox.value[textareabox.value.length - 1] == " ") {
+        reloadPopover(item, promptText)
+      }
+      
     }
 
     // save in local storage
     if (event.key == "Enter") {
       savePrompt(promptText)
+      promptText = ""
+      hidePopover()
     }
   });
 } 
@@ -53,12 +66,73 @@ export const addPromptSearchListener = () => {
 // save prompt
 export const savePrompt = (promptText : string) => {
   chrome.storage.local.get('prompts', function(result) {
-    var promptDict: { [key: string]: object } = {};
+    var promptDict: { [key: string]: {
+      answer: string,
+      usageCount: number,
+      lastUsed: Date
+    }} = {};
     if (result.prompts) {
       promptDict = JSON.parse(result.prompts)
     }
+
+    var observer = new MutationObserver(function(mutations) {
+      // for tracking promptDiv and answerDiv
+      var promptDiv: HTMLElement;
+      var answerDiv: HTMLElement;
+
+      mutations.forEach(function(mutation) {
+        var addedMutation = mutation.target as HTMLElement
+
+        // send button is live which means the record button has finished
+        if (addedMutation.className == 'absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1 md:bottom-2.5 md:right-2 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent') {
+            console.log('!!!!!!!!!!!className changed finished?');
+
+            try {
+              var promptDivText = promptDiv.childNodes[0].childNodes[1].childNodes[0].textContent
+              var answerDivText = answerDiv.childNodes[0].childNodes[1].childNodes[0].childNodes[0].childNodes[0] as HTMLElement
+              
+              if ((promptDivText == promptText) && promptDivText && promptDict[promptText] && promptDict[promptText]["answer"] == "Unavailable"
+                && answerDivText) {
+                console.log("WE'LL ADD THE ANSWER")
+
+                // code to add the answer
+                promptDict[promptText as string] = {
+                  answer: answerDivText.innerHTML,
+                  usageCount: 1,
+                  lastUsed: new Date()
+                }
+                chrome.storage.local.set({prompts: JSON.stringify(promptDict)})
+                console.log("end prompts: ", promptDict)
+              }
+            } catch {
+              console.log("something like a div didn't have enough nodes or something")
+            }
+        }
+        if (mutation.type === "childList") {
+          for (var i = 0; i < mutation.addedNodes.length; i++) {
+            var addedNode = mutation.addedNodes[i] as HTMLElement;
+            if (addedNode.nodeName === "DIV" && addedNode.className.includes('w-full border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800')) {
+              promptDiv = addedNode;
+              console.log("THIS IS THE PROMPT DIV: ", promptDiv)
+            } else if (addedNode.nodeName === "DIV" && addedNode.className.includes('w-full border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group bg-gray-50 dark:bg')) {
+              answerDiv = addedNode;
+              console.log("THIS IS THE ANSWER DIV: ", answerDiv)
+            }
+          }
+        }
+      });
+    });
+    var config = {
+      childList: true,
+      subtree: true
+    };
+    
+    observer.observe(document.body, config);
+
+
+    // default addition to local storage
     promptDict[promptText as string] = {
-      answer: "Not shared",
+      answer: "<p>Unavailable<p>",
       usageCount: 1,
       lastUsed: new Date()
     }
@@ -91,7 +165,7 @@ export const hidePopover = () => {
 
 // main code to show popup
 export const reloadPopover = (textbox : HTMLTextAreaElement, promptText : string) => {
-  // console.log("RELOADING popover")
+  console.log("RELOADING POPOVER")
   var p = document.getElementById("popover");
   if (p) {
     p.remove()
--- src/contentScript/popover.ts
+++ src/contentScript/popover.ts
@@ -1,3 +1,5 @@
+import { reloadPopover } from "./addPromptSearchListener";
+
 export const getPopover = (textbox : HTMLTextAreaElement, promptText : string) => {
   // Popover element
 
@@ -5,7 +7,8 @@ export const getPopover = (textbox : HTMLTextAreaElement, promptText : string) =
   popover.style.width = textbox.style.width;
   popover.style.height = "auto";
   popover.style.zIndex = "10";
-  popover.style.backgroundColor = "transparent";
+  popover.style.backgroundColor = "rgb(32,33,35)";
+  popover.style.opacity = "75%";
 
   popover.style.fontFamily = "sans-serif";
   popover.style.color = "rgb(210, 214, 218)";
@@ -13,13 +16,18 @@ export const getPopover = (textbox : HTMLTextAreaElement, promptText : string) =
   popover.style.display = "flex";
   popover.style.alignItems = "center";
   popover.style.justifyContent = "center";
-  popover.style.flexDirection = "column-reverse"
+  popover.style.flexDirection = "column"
   // popover.style.overflow = "hidden";
 
 
   // load in the suggestions
   chrome.storage.local.get('prompts', function(result) {
-    var promptDict: { [key: string]: object } = {};
+    var promptDict: { [key: string]: {
+      answer: string,
+      usageCount: number,
+      lastUsed: Date
+    }} = {};
+
     if (result.prompts) {
       promptDict = JSON.parse(result.prompts)
     }
@@ -32,6 +40,20 @@ export const getPopover = (textbox : HTMLTextAreaElement, promptText : string) =
     var add = true;
 
     // get promptMatchList which is all the prompts that should be presented
+    // TODO: sort promptDict based on how recent the entry was
+    // TODO: stop adding once you have 8
+    
+    // sort
+    Object.entries(promptDict).sort((a, b) => {
+      return a[1]['lastUsed'].valueOf() - b[1]['lastUsed'].valueOf()
+    })
+
+    // stored: [write me an essay about pyramids of giza, ...]
+    // search: write me essay
+    // 8 prompts that fully match words, sorted by most used
+    // x = promptText (search)
+
+    var counter = 0
     for (var [key, value] of Object.entries(promptDict)) {
       add = true
       for (const word of promptTextList) {
@@ -48,45 +70,74 @@ export const getPopover = (textbox : HTMLTextAreaElement, promptText : string) =
       }
       if (add) {
         promptMatchList.push([key, value])
+        counter += 1
+      }
+
+      if (counter > 7) {
+        break
       }
     }
 
     // add prompts to popover
     for (const [prompt, val] of promptMatchList) {
-      const suggestionBox = document.createElement("div");
-      suggestionBox.style.display = "flex"
-      suggestionBox.style.width = "100%"
-      suggestionBox.style.padding = "10px"
-      suggestionBox.style.opacity = "75%"
-      suggestionBox.style.backgroundColor = "rgb(32,33,35)";
-
-      suggestionBox.onmouseover = function() {
-        suggestionBox.style.cursor = "pointer"
-        suggestionBox.style.opacity = "100%"
-      }
-      suggestionBox.onmouseleave = function() {
-        suggestionBox.style.backgroundColor = "rgb(32,33,35)";
+      if (textbox.value != prompt.replaceAll("<b>", "").replaceAll("</b>", "")) {
+        const suggestionBox = document.createElement("div");
+        suggestionBox.style.display = "flex"
+        suggestionBox.style.width = "100%"
+        suggestionBox.style.padding = "10px"
         suggestionBox.style.opacity = "75%"
+        suggestionBox.style.backgroundColor = "rgb(32,33,35)";
+        suggestionBox.style.alignItems = "center"
+        
+
+        suggestionBox.onmouseover = function() {
+          suggestionBox.style.cursor = "pointer"
+          suggestionBox.style.opacity = "100%"
+        }
+        suggestionBox.onmouseleave = function() {
+          suggestionBox.style.backgroundColor = "rgb(32,33,35)";
+          suggestionBox.style.opacity = "75%"
+        }
+
+        suggestionBox.onclick = async function() {
+          var newText = prompt.replaceAll("<b>", "").replaceAll("</b>", "")
+          textbox.value = newText
+          reloadPopover(textbox, newText)
+          suggestionBox.remove()
+        }
+
+        const iconDiv = document.createElement("div");
+        iconDiv.style.marginRight = "10px"
+
+        const textWrapperDiv = document.createElement("div");
+
+
+        const textDiv = document.createElement("div");
+        textDiv.style.color = "white";
+        textDiv.style.fontFamily = "sans-serif";
+        textDiv.style.overflowX = "hidden";
+        textDiv.style.overflowY = "scroll";
+        textDiv.style.height = "25px";
+
+        const answerDiv = document.createElement("div");
+        answerDiv.style.fontFamily = "sans-serif";
+        answerDiv.style.overflow = "hidden";
+        answerDiv.style.marginLeft = "15px";
+        answerDiv.style.overflowX = "hidden";
+        answerDiv.style.overflowY = "scroll";
+        answerDiv.style.height = "25px";
+        answerDiv.innerHTML = val.answer;
+
+        textDiv.innerHTML = prompt
+        iconDiv.innerHTML = "🕓"
+
+        textWrapperDiv.appendChild(textDiv)
+        textWrapperDiv.appendChild(answerDiv)
+        suggestionBox.appendChild(iconDiv)
+        suggestionBox.appendChild(textWrapperDiv)
+        popover.appendChild(suggestionBox)
       }
-
-      suggestionBox.onclick = async function() {
-        var newText = prompt.replaceAll("<b>", "").replaceAll("</b>", "")
-        textbox.value = newText
-      }
-
-      const iconDiv = document.createElement("div");
-      iconDiv.style.marginRight = "10px"
-
-      const textDiv = document.createElement("div");
-
-      textDiv.innerHTML = prompt
-      iconDiv.innerHTML = "🕓"
-
-      suggestionBox.appendChild(iconDiv)
-      suggestionBox.appendChild(textDiv)
-      popover.appendChild(suggestionBox)
     }
-
   })
 
   popover.id = "popover";
